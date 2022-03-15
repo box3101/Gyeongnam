12f1d060875ce9024247a25519ebf1f3e18a9419
leechy91 <box4101@nate.com>
2022-03-15 16:06:13 +0900

    2022 03 15 수정사항 커밋

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,48 +73,16 @@ $(function () {
       }
     });
 
-    // 탭
-
-    // function comTab(){
-
-    //   // com var
-    //   var tabList = $(".tab__list");
-    //   var tabItem = $(".tab__item");
-    //   var tabCnt = $(".tab-content");
-
-    //   // init
-    //   tabCnt.hide();
-    //   tabCnt.eq(0).show();
-
-    //   // click event
-    //   tabList.on("click",function(e){
-    //     e.preventDefault();
-
-    //     var $this = $(this);
-    //     // 메뉴버튼
-    //     menuBtn($this);
-
-    //     // 콘텐츠변경
-    //     changeCnt($this);
-    //   });
-
-    //   function menuBtn($this){
-    //     tabList.find(tabItem).removeClass("is-active");
-    //     $this.find(tabItem).addClass("is-active");
-    //   }
-
-    //   function changeCnt($this){
-    //     var currentValue =  $this.find(tabItem).attr("href");
-
-    //     tabCnt.hide();
-    //     $(currentValue).show();
-    //   }
-
-    // }
 
     function init() {
       slideItem(); //슬라이드 모음
-      // comTab(); // 탭
+
+      // 날짜
+      $(".datePicker").each(function(){
+        $(this).datepicker({
+          autoHide: true,
+        });
+      });
     }
 
     init();
